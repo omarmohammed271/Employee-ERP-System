f434e48faa003808a7210d2dd9c451c2ce013e16
moooooora <omarmohammed271@gmail.com>
2024-06-28 11:27:22 +0300

    Added Retrieve old way

--- analysis.md
+++ analysis.md
@@ -9,3 +9,8 @@
     retrieve
     update
     delete    
+
+
+# Models -----> views ---urls--> template    
+# Models <-----> views ---urls--> template    
+# DB <--SQL---ORM-> Models <--Queryset---> views ---urls--> template    
--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Employee
 # Create your views here.
+
+
+## CRUD
+#R list---All employees
+def list_employees(request):
+    employees = Employee.objects.all()
+    context = {
+        'employees':employees,
+    }
+    return render(request,'employee_list.html',context)
+
+#R get---one employee
+def employee_detail(request,id):
+    try:
+        employee = Employee.objects.get(id=id)
+    ## SQL Injection
+    except:
+        return render(request,'404.html')
+    context = {
+        'employee':employee,
+    }
+    return render(request,'employee_detail.html',context)
